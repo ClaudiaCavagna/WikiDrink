18b09aff37af34b437ed1bfe6db8cc36aef0b3c5
ClaudiaCavagna <claudia.cavagna96@gmail.com>
2024-01-04 11:27:20 +0100

    project organization

---
+++ src/screen/ErrorScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+import Lottie from "react-lottie";
+import animationData from "../assets/animation/not-found.json";
+import { Link } from "react-router-dom";
+const ErrorScreen = () => {
+  return <>Error Screen</>;
+};
+
+
+export default ErrorScreen;
---
+++ src/screen/SingleCocktailScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Loading, ErrorMessage } from "../components";
+import { useParams, Link } from "react-router-dom";
+import { IoArrowBackCircleSharp } from "react-icons/io5";
+import styled from "styled-components";
+const SingleCocktailScreen = () => {
+  return <>Single Cocktail</>;
+};
+
+
+
+export default SingleCocktailScreen;
