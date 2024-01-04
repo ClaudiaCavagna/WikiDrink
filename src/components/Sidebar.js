23788bb6871bc4196c1c0b8ab2f3fa72804dbb2c
ClaudiaCavagna <claudia.cavagna96@gmail.com>
2024-01-04 17:25:53 +0100

    sidebar style

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { LinkComponent, SocialComponent } from '../utils/links';
+import { VscArrowSmallLeft } from 'react-icons/vsc';
 
 const Sidebar = () => {
   return (
-    <>
-      
-    </>
+    <aside className='sidebar show-sidebar'>
+      <header className='d-flex justify-content-between sidebar-container'>
+        <h3 className='nav-brand'>Wiki drink</h3>
+        <button>
+          <VscArrowSmallLeft />
+        </button>
+      </header>
+      <div className='sidebar-container'>
+        <LinkComponent classLink='sidebar-links'/>
+      </div>
+    </aside>
   )
 }
 
