138f1670e404227553da2469a7d006f16f981335
ClaudiaCavagna <claudia.cavagna96@gmail.com>
2024-01-06 17:57:13 +0100

    home screen style

--- src/screen/ContactScreen.js
+++ src/screen/ContactScreen.js
@@ -2,7 +2,55 @@ import React from "react";
 import { Hero } from "../components";
 import backImage from "../assets/images/Contact-hero.png";
 const ContactScreen = () => {
-  return <>ContactScreen</>;
+  return <>
+    <Hero img={backImage} disableOverlay>
+      <div className="contact-hero">
+        <div className="contact-hero-text">
+          <div className="contact-hero-title">
+            <h2>
+              Vorresti aggiungere un cocktail?
+            </h2>
+            <h4>
+              Il nostro team è sempre disponibile per valutare possibili nuove ricette ed ad aggiungerle alla nostro database
+            </h4>
+          </div>
+        </div>
+        <div className="contact-form-container">
+          <form 
+            action="https://formspree.io/f/mkndnyqb"
+            method="POST"
+            className="contact-form"
+          >
+            <div className="form-group">
+              <label htmlFor="name">Nome</label>
+              <input type="text" id="name" name="name" className="input" placeholder="Daniele" />
+              <hr/>
+            </div>
+            <div className="form-group">
+              <label htmlFor="surname">Cognome</label>
+              <input type="text" id="surname" name="surname" className="input" placeholder="Tamarindo" />
+              <hr />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email" className="input" placeholder="danitama@gmail.com" />
+              <hr />
+            </div>
+            <div className="form-group">
+              <label htmlFor="phone">Telefono</label>
+              <input type="cel" id="phone" name="phone" className="input" placeholder="555 555 5555" />
+              <hr />
+            </div>
+            <div className="form-group">
+              <label htmlFor="recipe">Ricetta</label>
+              <input type="textarea" id="recipe" name="recipe" className="input textarea" placeholder="Scrivi la tua ricetta qui..." />
+            </div>
+            <button className="btn btn-pink">Invia ricetta</button>
+          </form>
+        </div>
+      </div>
+    </Hero>
+  </>;
 };
 
 export default ContactScreen;
--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -5,7 +5,59 @@ import Lottie from "react-lottie";
 import animationData from "../assets/animation/drink-animation.json";
 import { Link } from "react-router-dom";
 const HomeScreen = () => {
-  return <>Home Screen</>;
+  const [input, setInput] = useState('tequila');
+  return <>
+  <Hero >
+    <div className="home-hero">
+      <div className="home-hero-text">
+        <div className="home-hero-title">
+          <h2>WIKI DRINK</h2>
+          <h4>Tutti i cocktail del mondo a portata di click</h4>
+          <p>Wiki Drink è un database internazionale che mette a tua disposizione, in maniera <span>Gratuita</span>, le ricette dei più importanti e diffusi cocktail al mondo.</p>
+          <Link to='/about' className='btn btn-pink'>
+            Scopri di più
+          </Link>
+        </div>
+      </div>
+      <div className="home-hero-img">
+        <Lottie 
+          options={{
+            loop: true,
+            autoplay: true,
+            reverse: true,
+            animationData,
+            rendererSettings: {
+              preserveAspectRatio: 'xMidYMid slice',
+            }
+          }}
+          height={350}
+        />
+      </div>
+    </div>
+  </Hero>
+  <section className="home-screen">
+    <div className="search-container">
+      <div className="form-container">
+        <form>
+          <label htmlFor="drink">
+            <h4>Cerca il tuo drink</h4>
+          </label>
+          <div className="search-bar">
+            <input id="drink" className="input"
+            placeholder={input} 
+            value={input}
+            onChange={(e)=>setInput(e.target.value)}
+            />
+            <button className="btn">
+              <FaSearch className="icon" />
+            </button>
+          </div>
+        </form>
+      </div>
+      <p className="result">3 risultati</p>
+    </div>
+  </section>
+  </>;
 };
 
 export default HomeScreen;
